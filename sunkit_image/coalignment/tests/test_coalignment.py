bfd9f7089d7d84f6edea26fc1386905dbfb648c0
Jayraj Dulange <jayraj.jayraj@iitgn.ac.in>
2024-08-14 12:32:23 +0000

    more simplified

--- sunkit_image/coalignment/tests/test_coalignment.py
+++ sunkit_image/coalignment/tests/test_coalignment.py
@@ -23,22 +23,10 @@ def is_test_map():
 
 
 @pytest.mark.remote_data()
-def aia193_test_map():
+def test_coalignment(is_test_map):
     date_start = "2014-01-08T09:57:27"
     date_end = "2014-01-08T10:00:00"
-    aia_map = Fido.fetch(Fido.search(a.Time(start=date_start, end=date_end), a.Instrument('aia'), a.Wavelength(193 * u.angstrom)))
-    return sunpy.map.Map(aia_map)
-
-
-@pytest.mark.remote_data()
-def aia193_test_shifted_map(aia193_test_map):
-    pixel_displacements = np.array([5.0, 5.0])
-    shifted_data = sp_shift(aia193_test_map.data, pixel_displacements)
-    return sunpy.map.Map(shifted_data, aia193_test_map.meta)
-
-
-@pytest.mark.remote_data()
-def test_coalignment(is_test_map, aia193_test_map):
+    aia193_test_map = sunpy.map.Map(Fido.fetch(Fido.search(a.Time(start=date_start, end=date_end), a.Instrument('aia'), a.Wavelength(193 * u.angstrom))))
     nx = (aia193_test_map.scale.axis1 * aia193_test_map.dimensions.x) / is_test_map.scale.axis1
     ny = (aia193_test_map.scale.axis2 * aia193_test_map.dimensions.y) / is_test_map.scale.axis2
     aia193_test_downsampled_map = aia193_test_map.resample(u.Quantity([nx, ny]))
